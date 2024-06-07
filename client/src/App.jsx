d3be32ee3885d56ea7b90a6b8929b7264388f600
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-06-07 15:29:26 +0700

    feat: Add loading message (Challenge)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,22 @@ import { useEffect, useState } from "react";
 import  axios  from "axios"
 import "./App.css";
 
-
 function App() {
 
   const [data, setData] = useState([])
+  const [loadingStatus, setLoadingStatus] = useState(false)
+  const [errorCatch, setError] = useState(false)
 
   const dataFetcher = async () => {
-    const getData = await axios.get("http://localhost:4001/products/")
-    setData(getData.data.data)
+    setLoadingStatus(true) 
+    try {
+      const getData = await axios.get("http://localhost:4001/products/")
+      setData(getData.data.data) 
+    } catch (error){
+      setError(true)
+    } finally {
+      setLoadingStatus(false) 
+    } 
   }
 
   useEffect(() => {dataFetcher()}, [])
@@ -20,35 +28,46 @@ function App() {
   }
 
   return (
+
     <div className="App">
+
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
-       {data.map((item,index) => {
-            return (
-            <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src={data[index].image}
-              alt="some product"
-              width="350"
-              height="350"
-            />
+      
+      { loadingStatus ? ( <h1 className="loading-message">Loading...</h1> 
+      ) : errorCatch ? ( <h1 className="loading-message">Fetching Error...</h1>  
+      ) : (
+      <div className="product-list">
+        {data.map((item) => (
+          <div key={item.id} className="product">
+            <div className="product-preview">
+              <img
+                src={item.image}
+                alt="some product"
+                width="350"
+                height="350"
+              />
+            </div>
+            <div className="product-detail">
+              <h1>Product name: {item.name}</h1>
+              <h2>Product price: {item.price} Baht</h2>
+              <p>Product description: {item.description}</p>
+            </div>
+            <button
+              className="delete-button"
+              onClick={() => {
+                deleteList(item.id);
+              }}
+            >
+              x
+            </button>
           </div>
-          <div className="product-detail">
-            <h1>Product name:{data[index].name}</h1>
-            <h2>Product price:{data[index].price} Baht</h2>
-            <p>Product description: {data[index].description}</p>
-          </div>
-          <button className="delete-button" onClick={() => {
-            deleteList(item.id)
-          }}>x</button>
-        </div>
-      </div>)
-          })}
+        ))}
+      </div>
+    )}
     </div>
-  );
+  )
 }
 
 export default App;
